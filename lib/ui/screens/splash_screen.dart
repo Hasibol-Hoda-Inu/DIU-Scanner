327d3151f6a68f61bfc22a9c82bc87dbb7478a96
Enu <hasinulhuda@gmail.com>
2025-01-13 00:37:47 +0600

    splash screen and navigation

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,13 +1,38 @@
+import 'package:diu_scanner/ui/screens/choose_template_screen.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   static const String name = "/";
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _goToTemplateScreen();
+  }
+  Future<void> _goToTemplateScreen()async {
+    await Future.delayed(Duration(seconds: 3));
+    if(mounted){
+      Navigator.pushNamedAndRemoveUntil(context, ChooseTemplateScreen.name, (predicate)=>false);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: Center(child: Text("DIU Scanner", style: Theme.of(context).textTheme.titleLarge))),
+          CircularProgressIndicator(),
+          const SizedBox(height: 36,)
+        ],
+      ),
     );
   }
 }
